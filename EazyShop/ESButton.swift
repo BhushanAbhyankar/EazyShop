9549eb6bf55cdfa80498939cb5d1a1c16b6924ae
Cesar Rojas <caesar.red@gmail.com>
2024-08-01 03:51:13 -0500

    Custom Button

---
+++ EazyShop/ESButton.swift
@@ -0,0 +1,47 @@
+//
+//  ESButtonStyle.swift
+//  EazyShop
+//
+//  Created by Cesar Rojas on 7/31/24.
+//
+
+import SwiftUI
+
+public struct ESButton<T: View>: View {
+    var titleKey: String = ""
+    var action: () -> Void
+    @ViewBuilder var label: () -> T
+   
+    init(titleKey: String = "", action: @escaping () -> Void, @ViewBuilder label: @escaping () -> T) {
+        self.titleKey = titleKey
+        self.action = action
+        self.label = label
+    }
+    
+    public var body: some View {
+        if titleKey.count > 0 {
+            Button(titleKey) {
+                buttonAction()
+            }
+        } else {
+            Button {
+                buttonAction()
+            } label: {
+                label()
+            }
+
+        }
+    }
+    
+    private func buttonAction() {
+        action()
+        debugPrint("---- \(self)")
+    }
+}
+
+public extension ESButton where T == EmptyView {
+    init(_ titleKey: String = "", action: @escaping () -> Void, extraLogs: [String: Any] = [:]) {
+        self.init(titleKey: titleKey, action: action, label: { EmptyView() })
+    }
+}
+
