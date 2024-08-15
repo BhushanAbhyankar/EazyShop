a7f1fefe19f37a0e442b2aae53cd0ef36807bff2
ebillson-grandjean <ebillson.grandjean@techconsulting.tech>
2024-08-15 14:17:51 -0400

    implemented tab navigation feature for the shopping app

---
+++ EazyShop/Views/Profiles/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  EazyShop
+//
+//  Created by Ebillson Grand Jean on 8/15/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
